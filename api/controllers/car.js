61f2d38a0f0c62832e18f3235433f6a7509bc628
albertorapu@gmail.com <albertorapu@gmail.com>
2022-08-24 18:01:11 -0400

    rama acr nueva

--- api/controllers/car.js
+++ api/controllers/car.js
@@ -1,41 +1,40 @@
-const mongoose = require("mongoose");
 const { Car } = require("../models");
 
 const carCtrl = {
-  addProductToCart: (req, res) => {
-    let newProduct = new Car({
-      products: req.body.products,
-      quantity: req.body.cant,
-      address: req.body.address,
-    });
+    addProductToCart: (req, res) => {
+        let newProduct = new Car({
+            products: req.body.products,
+            quantity: req.body.cant,
+            address: req.body.address,
+        });
 
-    newProduct.save().then((prod) => {
-      res.json(prod);
-    });
-  },
+        newProduct.save().then((prod) => {
+            res.json(prod);
+        });
+    },
 
-  findAllProductsInCart: (req, res) => {
-    Car.find({}).then((prods) => {
-      res.json(prods);
-    });
-  },
+    findAllProductsInCart: (req, res) => {
+        Car.find({}).then((prods) => {
+            res.json(prods);
+        });
+    },
 
-  deleteProductToCart: (req, res) => {
-    const { id } = req.params;
-    Car.findByIdAndRemove(id).then((result) => {
-      res.status(204).end();
-    });
-  },
+    deleteProductToCart: (req, res) => {
+        const { id } = req.params;
+        Car.findByIdAndRemove(id).then((result) => {
+            res.status(204).end();
+        });
+    },
 
-  updateProductToCart: (req, res) => {
-    const { id } = req.params;
+    updateProductToCart: (req, res) => {
+        const { id } = req.params;
 
-    const newCant = { quantity: req.body.cant };
+        const newCant = { quantity: req.body.cant };
 
-    Car.findByIdAndUpdate(id, newCant, { new: true }).then((result) => {
-      res.json(result);
-    });
-  },
+        Car.findByIdAndUpdate(id, newCant, { new: true }).then((result) => {
+            res.json(result);
+        });
+    },
 };
 
 module.exports = carCtrl;
