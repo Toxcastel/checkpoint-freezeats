1b61eb983c8cd5c74ad06865743b29c736a5152b
SteAymar <ste.aymar@gmail.com>
2022-08-26 21:21:25 -0300

    correcciones

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -22,7 +22,7 @@ const Home = () => {
 
   useEffect(() => {
     dispatch(handleProducts());
-  }, []);
+  }, [dispatch]);
 
   return (
     <>
--- src/views/App.js
+++ src/views/App.js
@@ -1,13 +1,11 @@
-import { Routes, Route, useNavigate } from "react-router-dom";
+import { Routes, Route} from "react-router-dom";
 import { useDispatch } from "react-redux";
-import { useEffect, useState } from "react";
+import { useEffect} from "react";
 import { logPersist } from "../store/reducers/userReducer.js";
 import Profile from "../components/Profile.jsx";
-import NavBar from "../components/Navbar.jsx";
-import Products from "../components/Products.jsx";
+import Navbar from "../components/Navbar.jsx";
 import Login from "../components/Login.jsx";
 import Signup from "../components/Signup.jsx";
-import Navbar from "../components/Navbar.jsx";
 import Home from "../components/Home.jsx";
 
 function App() {
@@ -24,8 +22,7 @@ function App() {
                 <Route path="/" element={<Home />} />
                 <Route path="/signup" element={<Signup />} />
                 <Route path="/login" element={<Login />} />
-                <Route path="/profile" element={<Profile />} />
-                <Route path="/:category" element={<Products />} />
+                <Route path="/profile" element={<Profile />} /> 
             </Routes>
         </div>
     );
